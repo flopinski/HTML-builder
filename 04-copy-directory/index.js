9392a930e09f530744923481ee374816bf419517
flopinski <103384762+flopinski@users.noreply.github.com>
2022-11-07 00:34:50 +0100

    feat: implement 4th part

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs'),
+  path = require('path'),
+  fsPromises = require('fs/promises');
+
+(function copyDirectory() {
+  fs.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true }, () => {
+
+    fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, () => {});
+   
+    fsPromises
+      .readdir(path.resolve(__dirname, 'files'), {withFileTypes: true})
+      .then(dirEntryList => {
+        dirEntryList.forEach(dirEntry => {
+          fsPromises.copyFile(
+            path.resolve(__dirname, 'files', dirEntry.name),
+            path.resolve(__dirname, 'files-copy', dirEntry.name)
+          );
+        });
+      })
+  });
+  
+
+})();
+
 
